54ac8e099d4bbb1c7fb3b4c2be16afcb0c0283d5
kaalcantara <kaalcantara@ICI-131030>
2018-08-10 16:39:35 -0300

    Add project files.

---
+++ MeuVoto/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeuVoto.Controllers
+{
+	public class HomeController : Controller
+	{
+		private string Logado { get; set; }
+
+		#region Eventos Get
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
+
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
+
+			return View();
+		}
+
+		public IActionResult Error()
+		{
+			return View();
+		}
+		public IActionResult Login()
+		{
+			return View();
+		}
+		public IActionResult Login([FromBody]object model)
+		{
+			return View();
+		}
+		#endregion
+
+		public void Votar()
+		{
+
+		}
+		[HttpPost]
+		public IActionResult Login([FromBody]object model)
+		{
+			return View();
+		}
+	}
+}
